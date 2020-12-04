ac09aee6bd9785cbd35d7d2ec10a0f391ae0dc49
Stigmond <stigmond@gmail.com>
2020-12-04 15:41:22 -0700

    Configure CrossOrigin

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/TestController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/TestController.java
@@ -1,9 +1,11 @@
 package com.skilldistillery.bbqueggle.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin({ "*", "http://localhost:4210" })  // NEED ALL THESE ANNOTATIONS FOR ALL CONTROLLERS
 @RestController
 @RequestMapping("api")
 
