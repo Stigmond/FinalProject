3109ae0b1f017531ceb7062f31cd8a9b656dfb55
David Daniels <dldaniels07@yahoo.com>
2020-12-06 16:36:57 -0700

    MainDish Service and ServieImpl

---
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/MainDishService.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.bbqueggle.services;
+
+import java.util.List;
+
+import com.skilldistillery.bbqueggle.entities.MainDish;
+
+
+public interface MainDishService {
+	
+List<MainDish> index();
+	
+	MainDish getMainDishById(Integer id);
+	
+	MainDish createMainDIsh(MainDish newMainDIsh);
+	
+	MainDish updateMainDish(Integer id, MainDish mainDish);
+	
+	boolean deleteMainDish(Integer id);
+
+}
---
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/MainDishServiceImpl.java
@@ -0,0 +1,39 @@
+package com.skilldistillery.bbqueggle.services;
+
+import java.util.List;
+
+import com.skilldistillery.bbqueggle.entities.MainDish;
+
+public class MainDishServiceImpl implements MainDishService {
+
+	@Override
+	public List<MainDish> index() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MainDish getMainDishById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MainDish createMainDIsh(MainDish newMainDIsh) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MainDish updateMainDish(Integer id, MainDish mainDish) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteMainDish(Integer id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
