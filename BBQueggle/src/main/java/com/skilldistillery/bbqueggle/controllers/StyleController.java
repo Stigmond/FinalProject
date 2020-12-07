62b0aa93fd3b3e110650b04bbef347c176879c4c
David Daniels <dldaniels07@yahoo.com>
2020-12-07 10:50:09 -0700

    StyleCOntroller formatting

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/StyleController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/StyleController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.skilldistillery.bbqueggle.entities.MainDish;
 import com.skilldistillery.bbqueggle.entities.Style;
 import com.skilldistillery.bbqueggle.services.StyleServiceImpl;
 
@@ -75,5 +75,15 @@ public class StyleController {
 			updatedStyle = null;
 		}
 		return updatedStyle;
+	}
+
+	@DeleteMapping("style/{styleId}")
+	public void deleteStyle(@PathVariable Integer styleId, HttpServletResponse response) {
+		if (svc.deleteStyle(styleId)) {
+			response.setStatus(204);
+
+		} else {
+			response.setStatus(404);
+		}
 	}
 }
