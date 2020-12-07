4e21b2b684288258691a117527a672781acee40e
David Daniels <dldaniels07@yahoo.com>
2020-12-07 13:31:36 -0700

    user repository

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/repositories/UserRepository.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.skilldistillery.bbqueggle.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.skilldistillery.bbqueggle.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 
 }
