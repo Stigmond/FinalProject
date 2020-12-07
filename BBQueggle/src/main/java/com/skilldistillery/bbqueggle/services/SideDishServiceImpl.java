2c2955126b789dd93c0175edbbb3fa634c309048
David Daniels <dldaniels07@yahoo.com>
2020-12-07 10:52:43 -0700

    added @Service annotation to sideDish serviceImpl

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/SideDishServiceImpl.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/SideDishServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.bbqueggle.entities.SideDish;
 import com.skilldistillery.bbqueggle.repositories.SideDishRepository;
 
+@Service
 public class SideDishServiceImpl implements SideDishService {
 
 	@Autowired
