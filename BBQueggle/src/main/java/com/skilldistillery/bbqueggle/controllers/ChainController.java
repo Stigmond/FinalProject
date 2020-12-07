8d83228be7e59623ea65320c21dd62f80b57ff3b
David Daniels <dldaniels07@yahoo.com>
2020-12-07 12:05:22 -0700

    chain controller index

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/ChainController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/ChainController.java
@@ -1,5 +1,27 @@
 package com.skilldistillery.bbqueggle.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.bbqueggle.entities.Chain;
+import com.skilldistillery.bbqueggle.services.ChainServiceImpl;
+
+@CrossOrigin({ "*", "http://localhost:4210" })
+@RequestMapping("api")
+@RestController
 public class ChainController {
+	
+	@Autowired 
+	private ChainServiceImpl svc;
+	
+	@GetMapping("chain")
+	public List<Chain> index() {
+		return svc.index();
+	}
 
 }
