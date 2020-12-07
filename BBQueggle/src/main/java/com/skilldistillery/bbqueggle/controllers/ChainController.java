00a69adfa117c9a4edfa1ca48b0767dbbcf331ef
David Daniels <dldaniels07@yahoo.com>
2020-12-07 12:12:43 -0700

    chaincontroller update and delete

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/ChainController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/ChainController.java
@@ -7,15 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.bbqueggle.entities.Chain;
-import com.skilldistillery.bbqueggle.entities.Style;
 import com.skilldistillery.bbqueggle.services.ChainServiceImpl;
 
 @CrossOrigin({ "*", "http://localhost:4210" })
@@ -57,6 +58,33 @@ public class ChainController {
 		}
 		return createdChain;
 
+	}
+
+	@PutMapping("chain/{chainId}")
+	public Chain updateChain(@PathVariable Integer chainId, @RequestBody Chain updatedChain,
+			HttpServletResponse response) {
+		try {
+			updatedChain = svc.updateChain(updatedChain, chainId);
+			if (updatedChain == null) {
+				response.setStatus(404);
+				updatedChain = null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(400);
+			updatedChain = null;
+		}
+		return updatedChain;
+	}
+
+	@DeleteMapping("chain/{chainId}")
+	public void deleteChain(@PathVariable Integer chainId, HttpServletResponse response) {
+		if (svc.deleteChain(chainId)) {
+			response.setStatus(204);
+
+		} else {
+			response.setStatus(404);
+		}
 	}
 
 }
