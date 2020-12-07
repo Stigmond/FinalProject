ea1a08ce3b4f8d9ef3173a56984b62875395c3fc
David Daniels <dldaniels07@yahoo.com>
2020-12-06 22:00:14 -0700

    mainDish Delete methods in Controller and ServiceImpl

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/MainDishController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/MainDishController.java
@@ -2,12 +2,17 @@ package com.skilldistillery.bbqueggle.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +40,54 @@ public class MainDishController {
 		}
 		return mainDish;
 	}
+
+	@PostMapping("mainDishes/{mainDishId}")
+	public MainDish createMainDish(@RequestBody MainDish newMainDish, HttpServletResponse response,
+			HttpServletRequest request) {
+		MainDish createdMainDish = null;
+		try {
+			createdMainDish = svc.createMainDish(newMainDish);
+			response.setStatus(201);
+			StringBuffer url = request.getRequestURL();
+			url.append("/").append(newMainDish.getId());
+			response.setHeader("Location", url.toString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(400);
+			newMainDish = null;
+		}
+		return createdMainDish;
+
+	}
+
+	@PutMapping("mainDishes/{mainDisheId}")
+	public MainDish updateMainDish(@PathVariable Integer mainDishId, @RequestBody MainDish updatedMainDish,
+			HttpServletResponse response) {
+		try {
+			updatedMainDish = svc.updateMainDish(mainDishId, updatedMainDish);
+			if (updatedMainDish == null) {
+				response.setStatus(404);
+				updatedMainDish = null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(400);
+			updatedMainDish = null;
+		}
+		return updatedMainDish;
+	}
+	
+
+	@DeleteMapping("mainDishes/{mainDishId}")
+	public void deleteMainDish(@PathVariable Integer mainDishId, HttpServletResponse response) {
+		if (svc.deleteMainDish(mainDishId)) {
+			response.setStatus(204);
+
+		} else {
+			response.setStatus(404);
+		}
+	}
+
 
 }
