2335311d3796ebf3b50eee861fa3e10d7945fde5
David Daniels <dldaniels07@yahoo.com>
2020-12-07 12:09:28 -0700

    chain controller createChain

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/ChainController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/ChainController.java
@@ -2,12 +2,15 @@ package com.skilldistillery.bbqueggle.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +38,25 @@ public class ChainController {
 			response.setStatus(404);
 		}
 		return chain;
+	}
+
+	@PostMapping("chain")
+	public Chain createChain(@RequestBody Chain newChain, HttpServletResponse response, HttpServletRequest request) {
+		Chain createdChain = null;
+		try {
+			createdChain = svc.createChain(newChain);
+			response.setStatus(201);
+			StringBuffer url = request.getRequestURL();
+			url.append("/").append(newChain.getId());
+			response.setHeader("Location", url.toString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			response.setStatus(400);
+			newChain = null;
+		}
+		return createdChain;
+
 	}
 
 }
