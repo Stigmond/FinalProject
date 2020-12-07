b5eae46dff0f4113d9710eff0da76a3754abb6d1
David Daniels <dldaniels07@yahoo.com>
2020-12-07 10:31:34 -0700

    StyleController index

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/StyleController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/StyleController.java
@@ -1,5 +1,27 @@
 package com.skilldistillery.bbqueggle.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.bbqueggle.entities.Style;
+import com.skilldistillery.bbqueggle.services.StyleServiceImpl;
+
+@CrossOrigin({ "*", "http://localhost:4210" })
+@RequestMapping("api")
+@RestController
 public class StyleController {
+
+	@Autowired
+	private StyleServiceImpl svc;
+
+	@GetMapping("style")
+	public List<Style> index() {
+		return svc.index();
+	}
 
 }
