5b6bbc62005f9aa814890ab302f6c09779d7b4b4
David Daniels <dldaniels07@yahoo.com>
2020-12-07 10:36:01 -0700

    StyleController getstylebyId

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/StyleController.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/controllers/StyleController.java
@@ -2,9 +2,12 @@ package com.skilldistillery.bbqueggle.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +25,15 @@ public class StyleController {
 	@GetMapping("style")
 	public List<Style> index() {
 		return svc.index();
+	}
+
+	@GetMapping("style/{styleId}")
+	public Style getStyleById(@PathVariable Integer styleId, HttpServletResponse response) {
+		Style style = svc.getStyleById(styleId);
+		if (style == null) {
+			response.setStatus(404);
+		}
+		return style;
 	}
 
 }
