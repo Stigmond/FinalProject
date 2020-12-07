db003aba0d2a98007de74bca5473d09e4093b41a
David Daniels <dldaniels07@yahoo.com>
2020-12-07 09:52:50 -0700

    style service

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/StyleService.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/StyleService.java
@@ -1,5 +1,18 @@
 package com.skilldistillery.bbqueggle.services;
 
+import java.util.List;
+
+import com.skilldistillery.bbqueggle.entities.Style;
+
 public interface StyleService {
 
+	List<Style>index();
+	
+	Style getStyleById(Integer Id);
+	
+	Style createStyle(Style newStyle);
+	
+	Style updateStyle(Style style, Integer Id);
+	
+	boolean deleteStyle(Integer Id);
 }
