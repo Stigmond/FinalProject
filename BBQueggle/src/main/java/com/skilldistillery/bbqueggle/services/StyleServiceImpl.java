9e95743305b4f536755e4cb86699aac466623ce6
David Daniels <dldaniels07@yahoo.com>
2020-12-07 09:54:11 -0700

    StyleServiceImpl

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/StyleServiceImpl.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/StyleServiceImpl.java
@@ -1,5 +1,42 @@
 package com.skilldistillery.bbqueggle.services;
 
-public class StyleServiceImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.bbqueggle.entities.Style;
+
+@Service
+public class StyleServiceImpl implements StyleService {
+
+	@Override
+	public List<Style> index() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Style getStyleById(Integer Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Style createStyle(Style newStyle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Style updateStyle(Style style, Integer Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteStyle(Integer Id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
