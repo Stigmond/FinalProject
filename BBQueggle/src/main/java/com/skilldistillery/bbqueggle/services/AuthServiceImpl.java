adb5320b973811d4ab059b78fdec47dfe8193799
David Daniels <dldaniels07@yahoo.com>
2020-12-10 16:19:11 -0700

    AuthServiceImpl

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/AuthServiceImpl.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/AuthServiceImpl.java
@@ -1,8 +1,21 @@
 package com.skilldistillery.bbqueggle.services;
 
-import com.skilldistillery.bbqueggle.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.skilldistillery.bbqueggle.entities.User;
+import com.skilldistillery.bbqueggle.repositories.UserRepository;
+
+@Service
 public class AuthServiceImpl implements AuthService{
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@Autowired
+	private UserRepository userRepo;
+	
 
 	@Override
 	public User register(User user) {
