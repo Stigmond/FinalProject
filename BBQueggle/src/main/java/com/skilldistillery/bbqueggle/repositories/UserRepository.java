18995394c6c06c91df45bc3f4c09cdad7cf3dd9d
David Daniels <dldaniels07@yahoo.com>
2020-12-10 16:12:26 -0700

    User repo, find by username

--- BBQueggle/src/main/java/com/skilldistillery/bbqueggle/repositories/UserRepository.java
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/repositories/UserRepository.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.skilldistillery.bbqueggle.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	User findByUsername (String username);
+	
+	User findByEmail(String email);
 
 }
---
+++ BBQueggle/src/main/java/com/skilldistillery/bbqueggle/services/AuthServiceImpl.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bbqueggle.services;
+
+import com.skilldistillery.bbqueggle.entities.User;
+
+public class AuthServiceImpl implements AuthService{
+
+	@Override
+	public User register(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
